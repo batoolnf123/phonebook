1d8b830c5590e5d04eaf7fb9de905c2e2109f093
joury <146828726+jourys@users.noreply.github.com>
2023-11-18 17:43:01 +0300

    Update BSTNode.java

--- src/phonebook/BSTNode.java
+++ src/phonebook/BSTNode.java
@@ -4,10 +4,26 @@
  */
 package phonebook;
 
-/**
- *
- * @author batoolalfouzan
- */
+
 public class BSTNode {
-    
+    //joury
+
+     public int key;
+public T data;
+public BSTNode<T> left, right;
+/** Creates a new instance of BSTNode */
+public BSTNode(int k, T val) {
+key = k;
+data = val;
+left = right = null;
+}
+public BSTNode(int k, T val, BSTNode<T> l, BSTNode<T> r) {
+key = k;
+data = val;
+left = l;
+right = r;
+}
+
+}
+   
 }
