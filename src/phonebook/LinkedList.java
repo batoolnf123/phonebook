b142620cede23d3a8e7cd459375aa239516465bf
joury <146828726+jourys@users.noreply.github.com>
2023-11-21 01:38:35 +0300

    Update LinkedList.java

--- src/phonebook/LinkedList.java
+++ src/phonebook/LinkedList.java
@@ -1,13 +1,6 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package phonebook;
 
-/**
- *
- * @author batoolalfouzan
- */
 public class LinkedList<T extends Comparable<T>> {
     class Node<T>{
         public T data;
