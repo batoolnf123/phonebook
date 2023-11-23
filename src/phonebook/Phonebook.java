47d034d371fdc8d80f2aa6fa0ec45b36a19c5cb9
joury <146828726+jourys@users.noreply.github.com>
2023-11-23 19:38:00 +0300

    Update Phonebook.java

--- src/phonebook/Phonebook.java
+++ src/phonebook/Phonebook.java
@@ -1,18 +1,45 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+
 package phonebook;
 
-/**
- *
- * @author batoolalfouzan
- */
-public class Phonebook {
 
-    /**
-     * @param args the command line arguments
-     */
+public class Phonebook {
+ static BST<Contact> AllContacts;
+    static BST<Event> AllEvent;
+
+    public PhoneBook() {//joury
+        AllContacts = new BST<Contact>();
+        AllEvent = new BST<Event>();
+    }
+
+    public void addContact(Contact newContact) {//joury
+        boolean add = false;
+        if (AllContacts.checkPhoneExist(newContact.phoneNumber)) {//check by phone number
+            System.out.println("this Contact phone is already exist");
+            return;
+        } else { //the phone number is not exist
+            add = AllContacts.insert(newContact.contactName, newContact);
+            if (!add) {
+
+                System.out.println("this Contact name is already exist");
+                return;
+            } else {
+                System.out.println("the contact added sucsessfuly");
+            }
+
+        }
+
+    }
+
+    public void printAllContactsInOrder() {//joury
+        AllContacts.inOrder();
+    }
+
+    public void printAllContactsPreOrder() {//joury
+        AllContacts.preOrder();
+    }
+
+}
+
     public static void main(String[] args) {
         // TODO code application logic here
     }
