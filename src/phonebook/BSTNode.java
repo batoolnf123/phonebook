852bf234b0bf1dc9ca5d0fcea4755d6ef17547e1
anoudmu <146832005+anoudmu@users.noreply.github.com>
2023-12-03 01:38:18 +0300

    Update BSTNode.java

--- src/phonebook/BSTNode.java
+++ src/phonebook/BSTNode.java
@@ -7,25 +7,20 @@ package phonebook;
 
 public class BSTNode<T> {
 
-    public String key;
+     public String key;
     public T data;
     public BSTNode<T> left, right;
 
-    public BSTNode() { //batool
-    }
-
-    public BSTNode(String k, T val) {//joury
+    public BSTNode(String k, T val) {
         key = k;
         data = val;
         left = right = null;
     }
 
-    public BSTNode(String k, T val, BSTNode<T> l, BSTNode<T> r) {//joury
+    public BSTNode(String k, T val, BSTNode<T> l, BSTNode<T> r) {
         key = k;
         data = val;
         left = l;
         right = r;
     }
-
-  
 }
